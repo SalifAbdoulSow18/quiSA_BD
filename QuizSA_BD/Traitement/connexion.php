1a9ed1582c68e52b3d3ae6088c939ebcb9bbe2ae
SalifAbdoulSow18 <62200280+SalifAbdoulSow18@users.noreply.github.com>
2020-05-25 00:24:12 -0100

    mon premier commit pour le quiz sas

---
+++ QuizSA_BD/Traitement/connexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+     <div class="milieu">
+     	<div class="first">
+     		<h4>WOULD YOU LIKE TO TEST QUIZ SAS?</h4>
+     	</div>
+     	<div class="formulaire">
+			<form method="POST" id="formulaire">
+				<input class="text" type="text" name="login" placeholder="Login" id="login">
+				<input class="text2" type="password" name="password" placeholder="Password" id="pass">
+				<input type="submit" name="submit" value="Se connecter" class="submit">
+				<a style="text-decoration: none;" href="creatjoueur.php"> <h3> S'inscrire</h3></a>
+			</form>
+     	</div>
+</body>
+</html>
---
+++ QuizSA_BD/Traitement/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>QUIZ SAS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>	
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+	<div class="contenaire-fluid haut">
+		<img src="../images/question.png">
+		<h1>QUIZ SAS</h1>
+		<img src="../images/question.png" class="logo">
+	</div>
+	<div class="conteneur">
+	<?php
+      	require_once("connexion.php");
+	?>
+	</div>
+	<div class="contenaire-fluid haut1">		
+		<h5>@Copyright SAS 2020</h5>
+	</div>
+	
+</body>
+</html>
