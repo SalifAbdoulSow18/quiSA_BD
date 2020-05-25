3f82d4741c3cf0b3de096060d83d66c2e64bdf9d
SalifAbdoulSow18 <62200280+SalifAbdoulSow18@users.noreply.github.com>
2020-05-25 00:46:12 -0100

    mes premiers commits avec quiz sas

--- QuizSA_BD/index.php
+++ QuizSA_BD/index.php
@@ -6,17 +6,17 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>	
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 	<div class="contenaire-fluid haut">
-		<img src="../images/question.png">
+		<img src="./images/question.png">
 		<h1>QUIZ SAS</h1>
-		<img src="../images/question.png" class="logo">
+		<img src="./images/question.png" class="logo">
 	</div>
 	<div class="conteneur">
 	<?php
-      	require_once("connexion.php");
+      	require_once("./Traitement/connexion.php");
 	?>
 	</div>
 	<div class="contenaire-fluid haut1">		
